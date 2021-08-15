54a2e0b91c5a0d3a91573217a1dc2c0bdd195475
AlonSchwierz <alon.schwierz@gmail.com>
2021-08-15 18:55:12 +0300

    AlonPot

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,15 +3,16 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Pot;
 
 public class Drivetrain extends SubsystemBase {
-    private TalonFX frMotor = new TalonFX(0);
-    private TalonFX rrMotor = new TalonFX(0);
-    private TalonFX flMotor = new TalonFX(0);
-    private TalonFX rlMotor = new TalonFX(0);
+    private TalonFX frMotor = new TalonFX(Pot.Drivetrain.Pot1);
+    private TalonFX rrMotor = new TalonFX(Pot.Drivetrain.Pot2);
+    private TalonFX flMotor = new TalonFX(Pot.Drivetrain.Pot3);
+    private TalonFX rlMotor = new TalonFX(Pot.Drivetrain.Pot4);
 
     public Drivetrain() {
-
+        frMotor.setInverted();
     }
 
     public void setPowerR(double valueMotorR) {
