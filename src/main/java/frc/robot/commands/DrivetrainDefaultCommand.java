5f46ecad582b622edfde09652960f6daadedd0fb
Gaia Zano <gaya.zano11@gmail.com>
2021-08-17 17:26:48 +0300

    add xbox values, add deadbands, set power

--- src/main/java/frc/robot/commands/DrivetrainDefaultCommand.java
+++ src/main/java/frc/robot/commands/DrivetrainDefaultCommand.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
 
@@ -15,7 +17,38 @@ public class DrivetrainDefaultCommand extends CommandBase {
     }
 
     @Override
-    public void execute() {
+    public void initialize() {
+        super.initialize();
+    }
 
+    @Override
+    public void execute() {
+        double rightPower = xboxController.getY();
+        double leftPower = xboxController.getY(GenericHID.Hand.kLeft);
+
+        double deadRight = deadband(rightPower);
+        double deadLeft = deadband(leftPower);
+
+        drivetrain.setLeftPower(deadLeft);
+        drivetrain.setRightPower(deadRight);
+    }
+
+    private double deadband(double value) {
+        if (value > Constants.Drivetrain.JOYSTICK_DEADBAND || value < -Constants.Drivetrain.JOYSTICK_DEADBAND) {
+            return value;
+        } else {
+            return 0;
+        }
+    }
+
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
     }
 }
