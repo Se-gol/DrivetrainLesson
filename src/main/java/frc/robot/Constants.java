e7abbf902e38200a7242a8c4958a859cb6280859
Gaia Zano <gaya.zano11@gmail.com>
2021-08-17 17:28:25 +0300

    Add JOYSTICK_DEADBAND

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import frc.robot.subsystems.Drivetrain;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final class Drivetrain {
+        public static  final double JOYSTICK_DEADBAND = 0.05;
+
+    }
+
+}
